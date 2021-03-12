cac93b3ce01f7af467e3a93107326e8d57024d2e
JustinasRa <71129390+JustinasRa@users.noreply.github.com>
2021-03-12 13:19:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# 1. Sukurti 4 skirtingus data tipus.
+ # data1 = false
+ # data2 = "no"
+ #data3 = 10
+ # data4 = 3.2
+# 2. Sukurti 3 skirtingas data struktūras su minimum 3 įrašais ir atvaizduoti kiekvieną struktūrą į atskira eilute.
+#def structure1():
+   # print("3")
+   # print("2")
+   # print("1")
+
+#structure1()
+# 3. Kokiu stilium rašomi kintamūjų pavadinimai iš dviejų ar daugiau žodžių python'e? pvz: MANO_KINTAMASIS = 1, Mano_Kintamasis = 2 ir t.t.
+# manoKintamasis  ; manokintamasis ; _mano_kintamasis ; manokintamasis2
+# 4. Kaip sukurti kintamajį reprezentuojantį float data tipą?
+# object1 = float(9.1)
+# 5. Parašyti funkcija kuri visada išprintins visus įrašus po vieną iš sarašo padavus bet kokį sarašą kaip parametrą.
+#  object2= [False,"test",3]
+#def array1(list):
+    #for x in list:
+        #print(x)
+#array1(list1)
+
+# 2 Dalis:
+#
+# 6. Kokias reikšmes turės x ir y kintamieji?
+# kintamasis = "IlgasSakinys"
+# x = kintamasis[2:5]
+# y = kintamasis[7:9]
+#  x =  Nuo g iki s (gaS)
+#  y = nuo k iki n (kin
+# 7. Kuo skiriasi mutable nuo not mutable data strukturos?
+# mutable gali buti pakeistas,o not mutable negali.
+# 8. Jeigu norėtumėm patobulinti mūsų funkcija kuri išspauzdina sarašo įrašus žinant, kad type(kintamasis) gražina kintamojo data tipą parašyti funkcija kuri patikrina ar parametras funkcijoje yra list ar dictionary ir išspauzdinti kiekvieną įrašą į atskira eilute.
